9498ca2dd48d2327690b4e39cf38e0b69df421df
Ines <lozano.lopez.ines@gmail.com>
2023-06-23 16:14:23 +0100

    feat: client components/router folders&files

---
+++ client/src/app/dashboard/index/page.tsx
@@ -0,0 +1,3 @@
+//List all surveys
+
+// new Survey button
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,4 +1,6 @@
-import Image from 'next/image'
+//LOG IN// Registration
+
+mport Image from 'next/image'
 import styles from './page.module.css'
 
 export default function Home() {
