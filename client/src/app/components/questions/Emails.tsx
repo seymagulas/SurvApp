3966db7e02e2212b037f59a1f9b9aa22d6f08469
Ines <lozano.lopez.ines@gmail.com>
2023-06-24 18:20:30 +0100

    ongoing: email

---
+++ client/src/app/components/questions/Emails.tsx
@@ -0,0 +1,48 @@
+'use client'
+import { useState } from "react";
+import {BsTrash} from "react-icons/bs";
+
+
+const Emails = () => {
+  const [emailsList, setEmailsList] = useState<string[]>([]);
+  const [inputEmails, setInputValues] = useState<string[]>([]);
+
+
+  const addInput = () => {
+    setInputValues([... inputValues, ""])
+  }
+
+  const addAnswer = (index:number, newTopic: string) => {
+    const updatedInputValues = [...inputValues];
+    updatedInputValues[index] = newTopic;
+    setAnswers(updatedInputValues);
+  }
+
+  const deleteAnswer = (TopicToDelete:string) => {
+    const updatedAnswers = answers.filter((topic) => topic !== TopicToDelete);
+    setAnswers(updatedAnswers);
+  }
+
+  const saveAnswers = () => {
+    setAnswers(inputValues)
+  }
+
+  return (
+    <div>
+      <div className="multiChoiceAddAnswer">
+        <input type="button" id="addAnswer" value="Add Answer" name="addAnswer" onClick={(addInput)}/>
+      </div>
+      <div className="MultiChoice-responses">
+        {inputValues.map((value, index) => (
+          <div key={value}>
+            <input type="text" value={value} onChange={(event) => addAnswer(index, event.target.value)}/>
+              <BsTrash onClick={() => deleteAnswer(value)}/>
+          </div>
+        ))}
+        <input type='submit' onClick={saveAnswers}/>
+      </div>
+    </div>
+  )
+}
+
+export default Emails
