63b07a5cb3d3ad810e8368a38bd394e169edea1c
Vipin Devan <vipindevan08@gmail.com>
2023-06-26 21:22:03 +0200

    feat: main page

--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['pastlenomad.github.io'],
+  },
+};
 
 module.exports = nextConfig;
