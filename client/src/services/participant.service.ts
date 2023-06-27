f906a023962da33fe2455a29ccd6f27265b2cadc
Seyma Gulas <seymatosunn@gmail.com>
2023-06-27 08:52:31 +0100

    feat: services implemented

---
+++ client/src/services/participant.service.ts
@@ -0,0 +1,66 @@
+'use strict';
+
+import axios from 'axios';
+import { toast } from 'react-toastify';
+import { IQuestion } from './survey.service';
+
+const API_URL = process.env.REACT_APP_API_BASE_URL;
+
+export interface IParticipantQuestion extends IQuestion {
+  isLastQuestion: boolean;
+}
+
+export interface IGetQuestionForParticipantRequest {
+  hash: string;
+}
+
+/**
+  - The getQuestionForParticipant will be used when the participant opens the
+  url that was sent by email.
+
+  - Example url: http://localhost:3001/survey?hash=d7ee2ab7fbf082438880bd1d78b50e6e5179f718
+
+  - This method also should be called after getting success response from the sendAnswer method.
+  Every time the getQuestionForParticipant called the next available question will be returned from the server.
+  When the server returns the last question, the isLastQuestion flag will be set to true. And the UI will be
+  able to show Continue/Finish buttons according to this flag's value.
+ */
+export const getQuestionForParticipant = async ({
+  hash,
+}: IGetQuestionForParticipantRequest): Promise<IParticipantQuestion> => {
+  try {
+    const response = await axios.get(`${API_URL}/survey/${hash}/participate`);
+    return response.data;
+  } catch (error) {
+    toast.error(error.response?.data.message);
+  }
+};
+
+export interface ISendAnswerRequest {
+  hash: string;
+  data: {
+    questionId: string;
+    answerId: string;
+    isFinished: boolean;
+  };
+}
+
+/**
+  - The sendAnswer will be used to send the participant's answer to the server.
+  
+  - questionId, answerId and isFinished values should be sent inside data object.
+  
+  - isFinished flag will be produced by the isLastQuestion flag that comes from the 
+  response of getQuestionForParticipant request.
+ */
+export const sendAnswer = async ({ hash, data }: ISendAnswerRequest) => {
+  try {
+    const response = await axios.post(
+      `${API_URL}/survey/${hash}/participate`,
+      data,
+    );
+    return response;
+  } catch (error) {
+    toast.error(error.response?.data.message);
+  }
+};
---
+++ client/src/services/survey.service.ts
@@ -0,0 +1,157 @@
+'use strict';
+
+import axios from 'axios';
+import { toast } from 'react-toastify';
+import { authHeader } from './auth.header';
+
+const API_URL = process.env.REACT_APP_API_BASE_URL;
+
+export interface IAnswerOption {
+  _id?: string;
+  text: string;
+  score?: number;
+}
+
+export const enum QuestionChoiceType {
+  multiChoice = 'multi-choice',
+  range = 'range',
+}
+
+export interface IQuestion {
+  _id?: string;
+  text: string;
+  choiceType: QuestionChoiceType;
+  answerOptions?: IAnswerOption[];
+}
+
+export const enum SurveyStatus {
+  new = 'new',
+  published = 'published',
+  completed = 'completed',
+}
+
+export interface ISurvey {
+  _id?: string;
+  userId: string;
+  name: string;
+  questions: IQuestion[];
+  status: SurveyStatus;
+}
+
+export const getAllSurveys = async (): Promise<ISurvey[]> => {
+  try {
+    const response = await axios.get(`${API_URL}/survey`, {
+      headers: authHeader(),
+    });
+    return response.data;
+  } catch (error) {
+    toast.error(error.response?.data.message);
+  }
+};
+
+export interface ISurveyRequest {
+  surveyId: string;
+}
+export const getSurvey = async ({
+  surveyId,
+}: ISurveyRequest): Promise<ISurvey> => {
+  try {
+    const response = await axios.get(`${API_URL}/survey/${surveyId}`, {
+      headers: authHeader(),
+    });
+    return response.data;
+  } catch (error) {
+    toast.error(error.response?.data.message);
+  }
+};
+
+export interface ICreateSurveyRequest {
+  data: ISurvey;
+}
+
+export const createSurvey = async ({
+  data,
+}: ICreateSurveyRequest): Promise<ISurvey> => {
+  try {
+    const response = await axios.post(`${API_URL}/survey`, data, {
+      headers: authHeader(),
+    });
+    return response.data;
+  } catch (error) {
+    toast.error(error.response?.data.message);
+  }
+};
+
+export interface IUpdateSurveyRequest {
+  data: ISurvey;
+  surveyId: string;
+}
+
+export const updateSurvey = async ({
+  data,
+  surveyId,
+}: IUpdateSurveyRequest): Promise<ISurvey> => {
+  try {
+    const response = await axios.put(`${API_URL}/survey/${surveyId}`, data, {
+      headers: authHeader(),
+    });
+    return response.data;
+  } catch (error) {
+    toast.error(error.response?.data.message);
+  }
+};
+
+export const deleteSurvey = async ({ surveyId }: ISurveyRequest) => {
+  try {
+    const response = await axios.delete(`${API_URL}/survey/${surveyId}`, {
+      headers: authHeader(),
+    });
+    return response;
+  } catch (error) {
+    toast.error(error.response.data.message);
+  }
+};
+
+export const publishSurvey = async ({ surveyId }: ISurveyRequest) => {
+  try {
+    const response = await axios.put(`${API_URL}/survey/${surveyId}/publish`, {
+      headers: authHeader(),
+    });
+    return response;
+  } catch (error) {
+    toast.error(error.response.data.message);
+  }
+};
+
+export const completeSurvey = async ({ surveyId }: ISurveyRequest) => {
+  try {
+    const response = await axios.put(`${API_URL}/survey/${surveyId}/complete`, {
+      headers: authHeader(),
+    });
+    return response;
+  } catch (error) {
+    toast.error(error.response.data.message);
+  }
+};
+
+export interface IShareSurveyRequest {
+  surveyId: string;
+  data: {
+    emails: string[];
+  };
+}
+
+export const shareSurvey = async ({ surveyId, data }: IShareSurveyRequest) => {
+  try {
+    const response = await axios.post(
+      `${API_URL}/survey/${surveyId}/share`,
+      data,
+      {
+        headers: authHeader(),
+      },
+    );
+    return response;
+  } catch (error) {
+    toast.error(error.response.data.message);
+  }
+};
