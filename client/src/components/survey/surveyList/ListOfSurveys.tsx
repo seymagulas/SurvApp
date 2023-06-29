c8fcf1225da06532b7f60952fba0691b3e268ce5
Ines <lozano.lopez.ines@gmail.com>
2023-06-29 07:15:53 +0100

    feat: refactor ListOfSurvey

--- client/src/components/survey/surveyList/ListOfSurveys.tsx
+++ client/src/components/survey/surveyList/ListOfSurveys.tsx
@@ -1,53 +1,61 @@
-'use client'
-import { useState, useEffect } from "react";
-import {getAllSurveys} from "../../../services/survey.service";
-import {ISurvey} from "../../../services/survey.service";
+'use client';
+​
+import { useState, useEffect } from 'react';
+import { getAllSurveys } from '../../../services/survey.service';
+import { ISurvey } from '../../../services/survey.service';
 import OptionsButtons from './OptionsButtons';
-
-
+​
 const ListOfSurveys = () => {
   const [surveys, setSurveys] = useState<ISurvey[]>([]);
-  const [optionsButtons, setOptionsButtons] = useState<Boolean>(false);
-
-
-  useEffect(()=> {
+  const [optionsButtons, setOptionsButtons] = useState<boolean>(false);
+​
+  const handleGetAllSurveys = () => {
     getAllSurveys()
-    .then((surveys: ISurvey[]) => {
-      setSurveys(surveys);
-    })
-    .catch((error: Error) =>{
-      console.log(error)
-    });
+      .then((surveys: ISurvey[]) => {
+        setSurveys(surveys);
+      })
+      .catch((error: Error) => {
+        console.log(error);
+      });
+  };
+​
+  useEffect(() => {
+    handleGetAllSurveys();
   }, []);
-
- 
-
+​
   const showOptionsButtons = () => {
-    setOptionsButtons(!optionsButtons)
-  }
-
+    setOptionsButtons(!optionsButtons);
+  };
+​
   return (
     <div>
       {surveys ? (
         <div>
-      <h2>Your Surveys</h2>
-      {surveys.map((survey) => (
-        <div key={survey._id}>
-          {survey.name}
-          <input type='button' onClick={() => showOptionsButtons} value='...'/>
-            {optionsButtons && (
-              <div>
-                <OptionsButtons setSurveys={setSurveys} survey={survey}/>
-              </div>
-            )}
+          <h2>Your Surveys</h2>
+          {surveys.map((survey) => (
+            <div key={survey._id}>
+              {survey.name}
+              <input
+                type="button"
+                onClick={() => showOptionsButtons}
+                value="..."
+              />
+              {optionsButtons && (
+                <div>
+                  <OptionsButtons
+                    handleGetAllSurveys={handleGetAllSurveys}
+                    survey={survey}
+                  />
+                </div>
+              )}
+            </div>
+          ))}
         </div>
-      ))}
-      </div>
       ) : (
         <p>No Surveys Created</p>
       )}
     </div>
-  )
-}
-
+  );
+};
+​
 export default ListOfSurveys;
