8b13f25ae6fabc28c62effbcd856ccac1d15b02a
Ines <lozano.lopez.ines@gmail.com>
2023-06-29 07:14:38 +0100

    feat: refactor OptionsButtons

--- client/src/components/survey/surveyList/OptionsButtons.tsx
+++ client/src/components/survey/surveyList/OptionsButtons.tsx
@@ -1,55 +1,61 @@
-import { Link } from "react-router-dom";
-import { ISurvey } from "../../../services/survey.service";
-import { completeSurvey, deleteSurvey, getAllSurveys, getSurvey, publishSurvey } from "../../../services/survey.service";
+'use client';
 
+import { useState, useEffect } from 'react';
+import { getAllSurveys } from '../../../services/survey.service';
+import { ISurvey } from '../../../services/survey.service';
+import OptionsButtons from './OptionsButtons';
 
-const OptionsButtons = ({ setSurveys, survey }) => {
+const ListOfSurveys = () => {
+  const [surveys, setSurveys] = useState<ISurvey[]>([]);
+  const [optionsButtons, setOptionsButtons] = useState<boolean>(false);
 
-  const deleteThisSurvey = () => {
-    deleteSurvey(survey._id)
-      .then(() => getAllSurveys())
-      .then((survey) =>
-        setSurveys([...survey]))
-  }
+  const handleGetAllSurveys= () => {
+    getAllSurveys()
+      .then((surveys: ISurvey[]) => {
+        setSurveys(surveys);
+      })
+      .catch((error: Error) => {
+        console.log(error);
+      });
+  };
 
-  const changeToPublish = async () => {
-    publishSurvey(survey._id)
-      .then(() => getSurvey(survey._id))
-      .then((survey: ISurvey) =>
-        setSurveys(survey))
-    }
-  const changeToComplete = async () => {
-    completeSurvey(survey._id)
-      .then(() => getSurvey(survey._id))
-      .then((survey: ISurvey) =>
-        setSurveys(survey))
-  }
-    
+  useEffect(() => {
+    handleGetAllSurveys();
+  }, []);
+
+  const showOptionsButtons = () => {
+    setOptionsButtons(!optionsButtons);
+  };
 
   return (
     <div>
-      {survey.status !== 'publish' || survey.status !== 'complete' && (
-        <Link to="/dashboard/survey">
-          <input type='button' value='Edit' />
-        </Link>
+      {surveys ? (
+        <div>
+          <h2>Your Surveys</h2>
+          {surveys.map((survey) => (
+            <div key={survey._id}>
+              {survey.name}
+              <input
+                type="button"
+                onClick={() => showOptionsButtons}
+                value="..."
+              />
+              {optionsButtons && (
+                <div>
+                  <OptionsButtons
+                    handleGetAllSurveys={handleGetAllSurveys}
+                    survey={survey}
+                  />
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
+      ) : (
+        <p>No Surveys Created</p>
       )}
-      {survey.status !== 'publish' && (
-        <input type='button' value='Publish' onClick={() => changeToPublish()} />
-      )}
-      <Link to="/dashboard/createNewSurvey/sendByEmail">
-        <input type='button' value='Share' />
-      </Link>
-      {survey.complete && (
-        <Link to="/dashboard/statistic">
-          <input type='button' value='Statistics' />
-        </Link>
-      )}
-      {survey.status === 'publish' && survey.status !== 'complete' && (
-        <input type='button' value='Complete' onClick={() => changeToComplete()} />
-      )}
-      <input type='button' value='Delete' onClick={() => deleteThisSurvey()} />
     </div>
-    )
-}
+  );
+};
 
-export default OptionsButtons;
+export default ListOfSurveys;
