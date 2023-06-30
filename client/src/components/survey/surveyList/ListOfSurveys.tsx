d27fec0a3c438f3691d6dd590b785124613c737b
Ines <lozano.lopez.ines@gmail.com>
2023-06-30 10:02:11 +0100

    feat: buttons logic done

--- client/src/components/survey/surveyList/ListOfSurveys.tsx
+++ client/src/components/survey/surveyList/ListOfSurveys.tsx
@@ -1,13 +1,26 @@
 'use client';
 
 import { useState, useEffect } from 'react';
-import { getAllSurveys } from '../../../services/survey.service';
+import {
+  getAllSurveys,
+  deleteSurvey,
+  completeSurvey,
+  publishSurvey,
+  SurveyStatus,
+} from '../../../services/survey.service';
 import { ISurvey } from '../../../services/survey.service';
-import OptionsButtons from './OptionsButtons';
+import {
+  RiDeleteBin6Line,
+  RiEdit2Fill,
+  RiShareLine,
+  RiFolderLockLine,
+  RiLockLine,
+} from 'react-icons/ri';
+import { useNavigate } from 'react-router-dom';
 
 const ListOfSurveys = () => {
+  const navigate = useNavigate();
   const [surveys, setSurveys] = useState<ISurvey[]>([]);
-  const [optionsButtons, setOptionsButtons] = useState<boolean>(false);
 
   const handleGetAllSurveys = () => {
     getAllSurveys()
@@ -23,38 +36,78 @@ const ListOfSurveys = () => {
     handleGetAllSurveys();
   }, []);
 
-  const showOptionsButtons = () => {
-    setOptionsButtons(!optionsButtons);
+  const handleOpenEdit = (surveyId: string) => {
+    // openEditQuestion(question);
+  };
+
+  const handleDelete = (id: string) => {
+    deleteSurvey({ surveyId: id }).then(() =>
+      setSurveys(surveys.filter((survey) => survey._id !== id)),
+    );
+  };
+
+  const handleComplete = (id: string) => {
+    completeSurvey({ surveyId: id }).then(() => handleGetAllSurveys());
+  };
+
+  const handlePublish = (id: string) => {
+    publishSurvey({ surveyId: id }).then(() => handleGetAllSurveys());
+    console.log(surveys);
   };
 
   return (
-    <div>
+    <>
       {surveys ? (
         <div>
-          <h2>Your Surveys</h2>
+          <h2 className="text-2xl font-bold mb-4">Your Surveys:</h2>
           {surveys.map((survey) => (
-            <div key={survey._id}>
-              {survey.name}
-              <input
-                type="button"
-                onClick={() => showOptionsButtons}
-                value="..."
-              />
-              {optionsButtons && (
-                <div>
-                  <OptionsButtons
-                    handleGetAllSurveys={handleGetAllSurveys}
-                    survey={survey}
-                  />
+            <div key={survey._id} className="question-div flex mb-2">
+              <div className="border border-gray-300 rounded-md p-2 flex-grow">
+                <div className="flex items-center justify-between">
+                  <p className="flex-grow-1">{survey.name}</p>
+                  <div className="flex justify-between">
+                    {survey.status === SurveyStatus.new && (
+                      <RiEdit2Fill
+                        className="action-button m1-2"
+                        onClick={() => {
+                          navigate(`/survey/${survey._id}/edit`);
+                        }}
+                      />
+                    )}
+                    <RiDeleteBin6Line
+                      className="action-button m1-2"
+                      onClick={() => handleDelete(survey._id ?? '')}
+                    />
+                    {survey.status === SurveyStatus.published && (
+                      <RiShareLine
+                        className="action-button m1-2"
+                        onClick={() => {
+                          navigate(`/survey/${survey._id}/send-by-email`);
+                        }}
+                      />
+                    )}
+                    {survey.status === SurveyStatus.new && (
+                      <RiFolderLockLine
+                        className="action-button m1-2"
+                        onClick={() => handlePublish(survey._id ?? '')}
+                      />
+                    )}
+                    {survey.status === SurveyStatus.published && (
+                      <RiLockLine
+                        className="action-button m1-2"
+                        onClick={() => handleComplete(survey._id ?? '')}
+                      />
+                    )}
+                  </div>
                 </div>
-              )}
+              </div>
             </div>
           ))}
         </div>
       ) : (
         <p>No Surveys Created</p>
       )}
-    </div>
+    </>
   );
 };
 
