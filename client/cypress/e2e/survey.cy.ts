33ad9af470e6d671b239008b1ce4f9c7a03b5894
Vipin Devan <vipindevan08@gmail.com>
2023-07-04 15:23:44 +0200

    feat: add options testing

--- client/cypress/e2e/survey.cy.ts
+++ client/cypress/e2e/survey.cy.ts
@@ -18,5 +18,22 @@ describe('Create Survey', () => {
     cy.get('select[aria-label="select type of question"]').select(
       'Multiple choice answers',
     );
+    cy.get('button[aria-label="Add option"]').click();
+    cy.get('input[aria-label="add answer option"]').type('Batman');
+    cy.get('button[aria-label="Add option"]').click();
+    cy.get('input[aria-label="add answer option"]').eq(1).type('Interstellar');
+    cy.get('button[aria-label="Add option"]').click();
+    cy.get('input[aria-label="add answer option"]').eq(2).type('Inception');
+    cy.get('button[aria-label="Add option"]').click();
+    cy.get('input[aria-label="add answer option"]').eq(3).type('Dunkrik');
+    cy.get('button[aria-label="Save question"]').click();
+    cy.get('button[aria-label="Add question"]').click();
+    cy.get('input[aria-label="add question"]').type('Favourite genre');
+    cy.get('select[aria-label="select type of question"]').select(
+      'Rating answers',
+    );
+    cy.get('button[aria-label="Save question"]').click();
+    cy.url().should('include', '/survey/new');
+    cy.get('button[aria-label="Save question"]').click();
   });
 });
